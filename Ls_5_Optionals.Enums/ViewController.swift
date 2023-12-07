596c35d52ccaa10eae34ec6ce73174a6ef66be86
Ilya Strotskiy <strotskiyi@list.ru>
2023-12-07 17:40:56 +0300

    initial commit

---
+++ Ls_5_Optionals.Enums/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ls_5_Optionals.Enums
+//
+//  Created by Strotskiysobaka on 03.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}
